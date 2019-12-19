410d749556f7b5820b1eadbe0733bf2c82c4768c
mochibot <amusaur83@gmail.com>
2019-12-18 21:33:30 -0800

    minor tweak

--- src/components/Main/About/index.jsx
+++ src/components/Main/About/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import Quotes from './Quotes';
 import Team from './Team';
 
 const About = () => {
   return (
-    <div>
+    <>
       <Quotes />
       <Team />
-    </div>
+    </>
   )
 }
 
